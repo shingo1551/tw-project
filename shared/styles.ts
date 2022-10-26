2344e3e915b2fd55bbae31c971dcc62f939fdd65
ShingoTanikawa <shingo.tanikawa@o-n.co.jp>
2022-10-26 21:01:23 +0900

    css` に統一

--- shared/styles.ts
+++ shared/styles.ts
@@ -1,7 +1,11 @@
-import { apply } from "twind";
 import { css } from "twind/css";
 
-export const btn = apply`bg-blue-200`;
-export const btn1 = css`{
+export const btn = css`
+{
+  @apply bg-blue-200;
+}`
+
+export const btn1 = css`
+{
   background-color: red;
-}`;
+}`
